646e1c85e82033549808e7108407e2d830f823ec
admin <admin@YD2-01>
2022-06-08 12:08:27 +0900

    부모자식 상속 연습

--- chap07/src/com/yedam/java/ch0702/Uncle.java
+++ chap07/src/com/yedam/java/ch0702/Uncle.java
@@ -6,7 +6,7 @@ public class Uncle extends GranPa {
 		System.out.println("Uncle 메소드");
 	}
 	
-	void methd3() {	//그랜파에 없는 메소드
+	void method3() {	//그랜파에 없는 메소드
 		System.out.println("Uncle 고유 메소드");
 	}
 }
