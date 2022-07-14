d0f4a07e99c5dc7c399dbd94411e31d8635fcc2c
admin <admin@YD2-01>
2022-07-14 17:52:06 +0900

    11720번

--- chap03/src/baekjun/P10430/Main.java
+++ chap03/src/baekjun/P10430/Main.java
@@ -8,15 +8,30 @@ public class Main {
 
 		Scanner scan = new Scanner(System.in);
 		
-		int a=scan.nextInt();
-		int b=scan.nextInt();
-		int c=scan.nextInt();
+//		int a=scan.nextInt();
+//		int b=scan.nextInt();
+//		int c=scan.nextInt();
+//		
+//		System.out.println((a+b)%c);
+//		System.out.println(((a%c)+(b%c))%c);
+//		System.out.println((a*b)%c);
+//		System.out.println(((a%c)*(b%c))%c);
 		
-		System.out.println((a+b)%c);
-		System.out.println(((a%c)+(b%c))%c);
-		System.out.println((a*b)%c);
-		System.out.println(((a%c)*(b%c))%c);
-
+		int len= scan.nextInt();	
+		int sum=0;
+		double num= scan.nextDouble();
+		double a=1;
+		int b=0;
+		for(int i=len;i>0;i--) {
+			for(int j=1;j<i;j++) {
+				a*=10;
+			}
+			b=(int) (num/a);
+			sum+=b;
+			num-=b*a;
+			a=1;
+		}	
+			System.out.println(sum);
 	}
-
+		
 }
